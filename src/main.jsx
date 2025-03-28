fe8167d2bf6a8e8b5529a2f229af2f554c2558ba
hp <riteshgharti121232@gmail.com>
2025-03-28 11:32:58 +0530

    remove snap

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
 import { StrictMode } from "react";
-import { createRoot, hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import "./styles/global.scss";
 
-const rootElement = document.getElementById("root");
-
-// ✅ Use hydration for pre-rendered HTML by react-snap
-if (rootElement.hasChildNodes()) {
-  // Hydrate pre-rendered content
-  hydrateRoot(rootElement, 
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-} else {
-  // Render normally for client-side rendering
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
